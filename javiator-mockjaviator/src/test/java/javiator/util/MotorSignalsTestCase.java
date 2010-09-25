d408fb701b8c86a1e6fd1d18b3c10f2780e91884
ckrainer <clemens.krainer@gmail.com>
2010-09-25 19:51:32 +0000

    MotorSignals now have 10 bytes. Test adapted.

--- javiator-mockjaviator/src/test/java/javiator/util/MotorSignalsTestCase.java
+++ javiator-mockjaviator/src/test/java/javiator/util/MotorSignalsTestCase.java
@@ -25,7 +25,7 @@ import junit.framework.TestCase;
 public class MotorSignalsTestCase extends TestCase {
 
 	public void testCase01 () {
-		byte [] payload = new byte[] {1,2,3,4,5,6,7,8};
+		byte [] payload = new byte[] {1,2,3,4,5,6,7,8,9,10};
 		
 		Packet packet = new Packet ((byte)212, payload);
 		MotorSignals actuatorData = new MotorSignals ();
